e659c1cc74f384e079f277f7bd631310293bd0ae
Mazen-Hossny-Ahmed <mazen.hossny.121@gmail.com>
2023-09-28 00:31:05 +0300

    Deploy

--- main.js
+++ main.js
@@ -35,26 +35,161 @@ closeBtn.onclick = () => {
   sidebar.style.transform = 'translate(250px)';
   overlay.style.display = 'none';
 }
+// window.onscroll = () => {
+
+//   if(scrollY >= 150) {
+//     h2.style.opacity =  1;
+//     p.style.opacity =  1;
+//     p.style.animation = 'fadeInUpWords 1s ease-in-out';
+//     h2.style.animation = 'fadeInUpWords 1s ease-in-out';
+//   }else {
+//     h2.style.animation = '';
+//     h2.style.opacity = '0';
+//     p.style.animation = '';
+//     p.style.opacity = '0';
+//   }
+//   if(scrollY >= 300) {
+//     title.style.animation = 'fadeInUpWords 1s ease-in-out';
+//     title.style.opacity = 1;
+//   }else {
+//     title.style.animation = '';
+//     title.style.opacity = 0;
+//   }
+//   if(scrollY >= 700) {
+//     boxOne.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxTwo.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxTwo.style.animationDelay = '0.2s';
+//     boxThree.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxThree.style.animationDelay = '0.4s';
+//     boxFour.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxFour.style.animationDelay = '0.6s';
+//     boxFive.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxFive.style.animationDelay = '0.8s';
+//     boxSix.style.animation = 'fadeInUp 1s ease-in-out';
+//     boxSix.style.animationDelay = '1s';
+//   }else {
+//     boxOne.style.animation = '';
+//     boxTwo.style.animation = '';
+//     boxTwo.style.animationDelay = '';
+//     boxThree.style.animation = '';
+//     boxThree.style.animationDelay = '';
+//     boxFour.style.animation = '';
+//     boxFour.style.animationDelay = '';
+//     boxFive.style.animation = '';
+//     boxFive.style.animationDelay = '';
+//     boxSix.style.animation = '';
+//     boxSix.style.animationDelay = '';
+//   }
+//   if(scrollY >= 1200) {
+//     main_button.style.animation = 'FadeLeft 1s ease-in-out';
+//   }else {
+//     main_button.style.animation = '';
+//   }
+//   if(scrollY >= 1837) {
+//     projectsTitle.style.animation = 'FadeRight 1s ease-in-out';
+//   }else {
+//     projectsTitle.style.animation = '';
+//   }
+//   if(scrollY >= 1952) {
+//     projectOne.style.animation = 'imagesFadeLeft 2s ease';
+//     projectTwo.style.animation = 'imagesFadeRight 2s ease';
+//   }else {
+//     projectOne.style.animation = '';
+//     projectTwo.style.animation = '';
+//   }
+//   if(scrollY >= 2416) {
+//     projectThree.style.animation = 'imagesFadeLeft 2s ease';
+//     projectFour.style.animation = 'imagesFadeRight 2s ease';
+//   }else {
+//     projectThree.style.animation = '';
+//     projectFour.style.animation = '';
+//   }
+//   if(scrollY >= 3062) {
+//     newsTitle.style.animation = 'fadeInUp 1s ease-in-out';
+//   }else {
+//     newsTitle.style.animation = '';
+//   }
+//   if(scrollY >= 3275) {
+//     newsBoxOne.style.animation = 'fadeInUp 1s ease-in-out';
+//     newsBoxTwo.style.animation = 'fadeInUp 1s ease-in-out';
+//     newsBoxTwo.style.animationDelay = '0.2s'
+//     newsBoxThree.style.animation = 'fadeInUp 1s ease-in-out';
+//     newsBoxThree.style.animationDelay = '0.4s'
+//   }else {
+//     newsBoxOne.style.animation = '';
+//     newsBoxTwo.style.animation = '';
+//     newsBoxThree.style.animation = '';
+
+//   }
+//   if(scrollY >= 3839) {
+//     newsButton.style.animation = 'FadeLeft 1s ease-in-out';
+//   }else {
+//     newsButton.style.animation = '';
+//   }
+//   if(scrollY >= 4515) {
+//     footerTitle.style.animation = 'fadeInUp 1s ease-in-out';
+//   }else {
+//     footerTitle.style.animation = '';
+//   }
+//   if(scrollY >= 5213) {
+//     mainFooterH3.style.animation = 'fadeInUp 1s ease-in-out';
+//     mainFooterButton.style.animation = 'FadeLeft 1s ease-in-out';
+//   }else {
+//     mainFooterH3.style.animation = '';
+//     mainFooterButton.style.animation = '';
+//   }
+// }
+
+
+
+
+
+let animationsTriggered = {
+  h2: false,
+  title: false,
+  boxes: false,
+  main_button: false,
+  projectsTitle: false,
+  projectImages: false,
+  newsTitle: false,
+  newsBoxes: false,
+  newsButton: false,
+  footerTitle: false,
+  mainFooterH3: false,
+  mainFooterButton: false,
+};
+
 window.onscroll = () => {
-  if(scrollY >= 150) {
-    h2.style.opacity =  1;
-    p.style.opacity =  1;
+  const scrollPosition = scrollY;
+
+  // Animation for h2 and p
+  if (scrollPosition >= 150 && !animationsTriggered.h2) {
+    h2.style.opacity = 1;
+    p.style.opacity = 1;
     p.style.animation = 'fadeInUpWords 1s ease-in-out';
     h2.style.animation = 'fadeInUpWords 1s ease-in-out';
-  }else {
+    animationsTriggered.h2 = true;
+  } else if (scrollPosition < 150) {
     h2.style.animation = '';
     h2.style.opacity = '0';
     p.style.animation = '';
     p.style.opacity = '0';
+    animationsTriggered.h2 = false;
   }
-  if(scrollY >= 300) {
+
+  // Animation for title
+  if (scrollPosition >= 300 && !animationsTriggered.title) {
     title.style.animation = 'fadeInUpWords 1s ease-in-out';
     title.style.opacity = 1;
-  }else {
+    animationsTriggered.title = true;
+  } else if (scrollPosition < 300) {
     title.style.animation = '';
     title.style.opacity = 0;
+    animationsTriggered.title = false;
   }
-  if(scrollY >= 700) {
+
+  // Animation for boxes
+  if (scrollPosition >= 700 && !animationsTriggered.boxes) {
     boxOne.style.animation = 'fadeInUp 1s ease-in-out';
     boxTwo.style.animation = 'fadeInUp 1s ease-in-out';
     boxTwo.style.animationDelay = '0.2s';
@@ -66,7 +201,8 @@ window.onscroll = () => {
     boxFive.style.animationDelay = '0.8s';
     boxSix.style.animation = 'fadeInUp 1s ease-in-out';
     boxSix.style.animationDelay = '1s';
-  }else {
+    animationsTriggered.boxes = true;
+  } else if (scrollPosition < 700) {
     boxOne.style.animation = '';
     boxTwo.style.animation = '';
     boxTwo.style.animationDelay = '';
@@ -78,63 +214,93 @@ window.onscroll = () => {
     boxFive.style.animationDelay = '';
     boxSix.style.animation = '';
     boxSix.style.animationDelay = '';
+    animationsTriggered.boxes = false;
   }
-  if(scrollY >= 1200) {
+
+  // Animation for main_button
+  if (scrollPosition >= 1200 && !animationsTriggered.main_button) {
     main_button.style.animation = 'FadeLeft 1s ease-in-out';
-  }else {
+    animationsTriggered.main_button = true;
+  } else if (scrollPosition < 1200) {
     main_button.style.animation = '';
+    animationsTriggered.main_button = false;
   }
-  if(scrollY >= 1837) {
+
+  // Animation for projectsTitle
+  if (scrollPosition >= 1837 && !animationsTriggered.projectsTitle) {
     projectsTitle.style.animation = 'FadeRight 1s ease-in-out';
-  }else {
+    animationsTriggered.projectsTitle = true;
+  } else if (scrollPosition < 1837) {
     projectsTitle.style.animation = '';
+    animationsTriggered.projectsTitle = false;
   }
-  if(scrollY >= 1952) {
+
+  // Animation for projectImages
+  if (scrollPosition >= 1952 && !animationsTriggered.projectImages) {
     projectOne.style.animation = 'imagesFadeLeft 2s ease';
     projectTwo.style.animation = 'imagesFadeRight 2s ease';
-  }else {
-    projectOne.style.animation = '';
-    projectTwo.style.animation = '';
-  }
-  if(scrollY >= 2416) {
     projectThree.style.animation = 'imagesFadeLeft 2s ease';
     projectFour.style.animation = 'imagesFadeRight 2s ease';
-  }else {
+    animationsTriggered.projectImages = true;
+  } else if (scrollPosition < 1952) {
+    projectOne.style.animation = '';
+    projectTwo.style.animation = '';
     projectThree.style.animation = '';
     projectFour.style.animation = '';
+    animationsTriggered.projectImages = false;
   }
-  if(scrollY >= 3062) {
+
+  // Animation for newsTitle
+  if (scrollPosition >= 2416 && !animationsTriggered.newsTitle) {
     newsTitle.style.animation = 'fadeInUp 1s ease-in-out';
-  }else {
+    animationsTriggered.newsTitle = true;
+  } else if (scrollPosition < 2416) {
     newsTitle.style.animation = '';
+    animationsTriggered.newsTitle = false;
   }
-  if(scrollY >= 3275) {
+
+  // Animation for newsBoxes
+  if (scrollPosition >= 3275 && !animationsTriggered.newsBoxes) {
     newsBoxOne.style.animation = 'fadeInUp 1s ease-in-out';
     newsBoxTwo.style.animation = 'fadeInUp 1s ease-in-out';
-    newsBoxTwo.style.animationDelay = '0.2s'
+    newsBoxTwo.style.animationDelay = '0.2s';
     newsBoxThree.style.animation = 'fadeInUp 1s ease-in-out';
-    newsBoxThree.style.animationDelay = '0.4s'
-  }else {
+    newsBoxThree.style.animationDelay = '0.4s';
+    animationsTriggered.newsBoxes = true;
+  } else if (scrollPosition < 3275) {
     newsBoxOne.style.animation = '';
     newsBoxTwo.style.animation = '';
     newsBoxThree.style.animation = '';
+    animationsTriggered.newsBoxes = false;
+  }
 
-  }
-  if(scrollY >= 3839) {
+  // Animation for newsButton
+  if (scrollPosition >= 3839 && !animationsTriggered.newsButton) {
     newsButton.style.animation = 'FadeLeft 1s ease-in-out';
-  }else {
+    animationsTriggered.newsButton = true;
+  } else if (scrollPosition < 3839) {
     newsButton.style.animation = '';
+    animationsTriggered.newsButton = false;
   }
-  if(scrollY >= 4515) {
+
+  // Animation for footerTitle
+  if (scrollPosition >= 4515 && !animationsTriggered.footerTitle) {
     footerTitle.style.animation = 'fadeInUp 1s ease-in-out';
-  }else {
+    animationsTriggered.footerTitle = true;
+  } else if (scrollPosition < 4515) {
     footerTitle.style.animation = '';
+    animationsTriggered.footerTitle = false;
   }
-  if(scrollY >= 5213) {
+
+  // Animation for mainFooterH3 and mainFooterButton
+  if (scrollPosition >= 5213 && !animationsTriggered.mainFooterH3) {
     mainFooterH3.style.animation = 'fadeInUp 1s ease-in-out';
     mainFooterButton.style.animation = 'FadeLeft 1s ease-in-out';
-  }else {
+    animationsTriggered.mainFooterH3 = true;
+  } else if (scrollPosition < 5213) {
     mainFooterH3.style.animation = '';
     mainFooterButton.style.animation = '';
+    animationsTriggered.mainFooterH3 = false;
   }
-}
+
+};
